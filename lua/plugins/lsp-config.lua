e6924b9df2499d655ba85390cc8ceb8cc4308a66
Arbint <cg.jingtianli@gmail.com>
2024-05-27 21:15:22 -0500

    added lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     'williamboman/mason-lspconfig.nvim',
     config = function()
       require('mason-lspconfig').setup({
-        ensure_installed = { 'lua_ls', 'clangd', 'pylsp' }
+        ensure_installed = { 'lua_ls', 'clangd', 'pylsp', 'cmake'}
       })
     end
   },
